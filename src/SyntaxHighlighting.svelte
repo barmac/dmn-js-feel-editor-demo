ef28be5333e9a6e5462b0c4a6c4f632ae5d07642
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2020-02-27 18:01:23 +0100

    feat(project): configure syntax highlighting

--- src/SyntaxHighlighting.svelte
+++ src/SyntaxHighlighting.svelte
@@ -2,12 +2,34 @@
   import { onDestroy, onMount } from 'svelte';
   import DmnModeler from 'dmn-js/lib/Modeler';
 
+  import {
+    DecisionTableSyntaxHighlighting,
+    LiteralExpressionSyntaxHighlighting
+  } from 'dmn-js-syntax-highlighting';
+
   export let xml;
 
   let modeler;
 
   onMount(() => {
-    modeler = new DmnModeler({ container: '#container' });
+    modeler = new DmnModeler({
+      common: {
+        codeEditor: {
+          autocomplete: false
+        }
+      },
+      decisionTable: {
+        additionalModules: [
+          DecisionTableSyntaxHighlighting
+        ]
+      },
+      literalExpression: {
+        additionalModules: [
+          LiteralExpressionSyntaxHighlighting
+        ]
+      },
+      container: '#container'
+    });
 
     modeler.importXML(xml);
   });
