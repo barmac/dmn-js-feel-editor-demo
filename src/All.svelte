12a3e5b05cadd7eeaf8473c57af0f5cb13be3fec
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2020-02-27 17:30:04 +0100

    chore(project): init

---
+++ src/All.svelte
@@ -0,0 +1,35 @@
+<script>
+  import { onDestroy, onMount } from 'svelte';
+  import DmnModeler from 'dmn-js/lib/Modeler';
+
+  import {
+    DecisionTableSyntaxHighlighting,
+    LiteralExpressionSyntaxHighlighting
+  } from 'dmn-js-syntax-highlighting';
+
+  export let xml;
+
+  let modeler;
+
+  onMount(() => {
+    modeler = new DmnModeler({
+      decisionTable: {
+        additionalModules: [
+          DecisionTableSyntaxHighlighting
+        ]
+      },
+      literalExpression: {
+        additionalModules: [
+          LiteralExpressionSyntaxHighlighting
+        ]
+      },
+      container: '#container'
+    });
+
+    modeler.importXML(xml);
+  });
+
+  onDestroy(() => modeler.destroy());
+</script>
+
+<div id="container"></div>
